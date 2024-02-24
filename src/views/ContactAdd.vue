a063b0906bd2016b15418c1caeec899d105a7dc2
phathuynh2082002 <142472879+phathuynh2082002@users.noreply.github.com>
2024-02-24 20:51:43 +0700

    Tao trang them  lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="page">
+    <h4>Thêm Liên hệ</h4>
+    <ContactForm :contact="{}" @submit:contact="addContact"/>
+    <p>{{ message }}</p>
+  </div>
+</template>
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+export default {
+  components: {
+    ContactForm,
+  },
+  data() {
+    return {
+      message: "",
+    };
+  },
+  methods: {
+    async addContact(data) {
+      try {
+        await ContactService.create(data);
+        this.message = "Liên hệ được tạo thành công.";
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
